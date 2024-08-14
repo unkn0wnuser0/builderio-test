6ddfa93741cbb19fdaba5e66c5034aa3d1a09966
thereisnosp00n <105610858+thereisnosp00n@users.noreply.github.com>
2024-08-14 13:33:23 +0300

    Init0

---
+++ MyComponent.tsx
@@ -0,0 +1,169 @@
+import * as React from "react";
+import styles from './MyComponent.module.css';
+
+function MyComponent() {
+  return (
+    <>
+      <div className={styles.26LightSleepTracker}>
+        <div className={styles.typeStatusBarThemeLightComponentStatusBar}>
+          <div className={styles.941}>9:41</div>
+          <div className={styles.div}>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/29dd8a033babe1a996bdbfaad61ec53ee29d36042767c31958fc574b1d9df813?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+              className={styles.img}
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/da781bfa01abe5b80099f6e7695468cbdec0ae2171b65fb294b7a71fc4c88353?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+              className={styles.img2}
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/aca012a775f2fbad6bb9d2b62da49effb29625ea45aa480d4c3452b4e825a3e6?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+              className={styles.img3}
+            />
+          </div>
+        </div>
+        <div className={styles.navbarNavbarFullDarkFalseComponentNavbar}>
+          <div className={styles.navbarDefaultDarkFalseComponentNavbar}>
+            <div className={styles.autoLayoutHorizontal}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/87098f2a49f67360367a8c8b04dd30b94f7b9d61f8d8e381fe8d68a55a48524a?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img4}
+              />
+            </div>
+            <div className={styles.titleSection}>Home</div>
+            <div className={styles.autoLayoutHorizontal2}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/c358b15596c44ba8f29af5aea41cf66794a241ffe73dda76d917dfdd6e90a6b5?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img5}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.autoLayoutVertical}>
+          <div className={styles.autoLayoutVertical2}>
+            <div className={styles.elementSetSleepTime1DarkFalseComponentHomeTracker}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/1db829e98d0d45f21c097f156ca6c93f6361a1667024271387d069e1b37917e8?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img6}
+              />
+            </div>
+            <div className={styles.elementBedtimeWakeUpDarkFalseComponentHomeTracker}>
+              <div className={styles.autoLayoutVertical3}>
+                <div className={styles.autoLayoutHorizontal3}>
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/75dae4a6bdbd852a92f74e0e4dc8c8d6f982ca7e4244a25e9e80d1db97bf453d?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                    className={styles.img7}
+                  />
+                  <div className={styles.bedtime}>Bedtime</div>
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/3ee150a976927fa4ee29f4f1cce43edc4d50f3e5f108cd963fba67c64e01c111?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                    className={styles.img8}
+                  />
+                </div>
+                <div className={styles.2230}>22:30</div>
+              </div>
+              <div className={styles.autoLayoutVertical4}>
+                <div className={styles.autoLayoutHorizontal4}>
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/f3561968a7de155afa43d08f75cd12ec4954c3e058c273d5688492b07efda4a9?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                    className={styles.img9}
+                  />
+                  <div className={styles.alarm}>Alarm</div>
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/0cf823a7fcfda2254d298e9ef28a4f9186701a0ba660a4b6596c95a60f2c83dd?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                    className={styles.img10}
+                  />
+                </div>
+                <div className={styles.05300600}>05:30 - 06:00</div>
+              </div>
+            </div>
+            <div className={styles.elementAlarmRepeatDarkFalseComponentHomeTracker}>
+              <div className={styles.elementsSettingsList1DarkFalseComponentAdditionalElements}>
+                <div className={styles.settings}>Alarm Sound</div>
+                <div className={styles.settings2}>Sunrise Serenade</div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/0594d6e754a3f5c4217948379c2a72f4eb25d4b1460b3a0eb7cb85f59195b7f8?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                  className={styles.img11}
+                />
+              </div>
+              <div className={styles.darkFalseComponentDivider}>
+                <div className={styles.line} />
+              </div>
+              <div className={styles.elementsSettingsList1DarkFalseComponentAdditionalElements2}>
+                <div className={styles.settings}>Repeat</div>
+                <div className={styles.settings3}>Everyday</div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/0594d6e754a3f5c4217948379c2a72f4eb25d4b1460b3a0eb7cb85f59195b7f8?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                  className={styles.img12}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.buttonPillTypePillIconFalseSizeMediumStylePillSocialMediaNoneDisabledFalseThemeDefaultComponentButton}>
+            Sleep Now
+          </div>
+        </div>
+        <div className={styles.activeMenuHomeDarkFalseComponentMenuBar}>
+          <div className={styles.autoLayoutHorizontal5}>
+            <div className={styles.autoLayoutVertical5}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7fe2024a26b6083117fb6e84f0d6e6a8d9a2f26142eda05a7f1e1f5bdc58a2fe?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img13}
+              />
+              <div>Home</div>
+            </div>
+            <div className={styles.autoLayoutVertical6}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/cc4a738faeb21f8632ff619e91103ae860e132281ae3e72dc74749f4da7dd3eb?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img14}
+              />
+              <div>Sounds</div>
+            </div>
+            <div className={styles.autoLayoutVertical7}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/306da5542ec4e841ab465a2a0961b668f7276ed9531244c53c9844ab629dd7ae?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img15}
+              />
+              <div>Journal</div>
+            </div>
+            <div className={styles.autoLayoutVertical8}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e16f4c053dca83836b2a7fd5fd21bb94f0d76dab48d6a69806392fc67f5d6dc8?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img16}
+              />
+              <div>Report</div>
+            </div>
+            <div className={styles.autoLayoutVertical9}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/650671a3dafbe2e642f6adb390965aee1ce61b43f68cbbf7fd46f2f93e31b69f?placeholderIfAbsent=true&apiKey=251352a7bd68494aa49aac5efe035645"
+                className={styles.img17}
+              />
+              <div>Account</div>
+            </div>
+          </div>
+          <div className={styles.darkFalse}>
+            <div className={styles.vector} />
+          </div>
+        </div>
+      </div>
+      
+    </>
+  );
+}
